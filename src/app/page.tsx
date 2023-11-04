5bae0007ec17ef518baf18004bd4efd95511dddb
Kynderman Baraldi <kynderman@gmail.com>
2023-11-04 10:54:54 -0300

    update initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
+import { GiPrayer } from "react-icons/gi";
+
 export default function Home() {
   return (
     <div className="home-body">
 
-      <h1>Igreja Assembleia do Araretama</h1>
-    
+      <section className="home-top">
+        <h1>Igreja Assembleia do Araretama</h1>
+        <h3>Nossa missão é compartilhar a mensagem transformadora do Evangelho com amor e humildade.</h3>
+      </section>
+      <section className="home-midle">
+        <Link href="#" id="button-junte-se">Junte-se a nós.</Link>
+      </section>
+      <section className="home-botton">
+        <p>Cada pessoa é valiosa aos olhos de Deus.</p>
+        <p>Todos são bem-vindos, independentemente de sua história, origem ou situação.</p>
+        <p>Vejam suas vidas refletirem o amor de Deus e impactar positivamente o mundo ao seu redor.</p>
+        <GiPrayer id="praying"/>
+
+      </section>
+
     </div>
   )
 }
