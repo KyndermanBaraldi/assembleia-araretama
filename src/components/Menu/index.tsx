f704ffa11978f1262e814209adac6a09d41cabb4
Kynderman Baraldi <kynderman@gmail.com>
2023-11-04 09:01:50 -0300

    add menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,98 @@
+'use client'
+import Link from "next/link";
+import { useState } from "react";
+import { AiFillYoutube, AiFillInstagram } from "react-icons/ai";
+import { FaHandsPraying } from "react-icons/fa6"
+import { VscMenu, VscChromeClose } from "react-icons/vsc";
+
+const Menu: React.FC = () => {
+
+    const [menuOpen, setMenuOpen] = useState(true);
+  
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
+  
+    const closeMenu = () => {
+      setMenuOpen(true);
+    };
+        
+    return (
+      <nav className="menu-navbar">
+        <div className="menu-logo">
+          <Link href="/" onClick={closeMenu}>
+                <FaHandsPraying/>
+          </Link>
+        </div>
+        <div className={`nav-list ${menuOpen ? "hide" : ""}`}>
+          <ul>
+            <li className="nav-item">
+              <Link href="/" className="nav-link" onClick={closeMenu}>
+                Início
+              </Link>
+            </li>
+             
+             <li className="dropdown">
+              <a className="nav-link" href="#" onClick={closeMenu}>Sobre</a>  
+              <div className="dropdown-menu">
+                <Link href="#" className="nav-link" onClick={closeMenu}>
+                  História
+                </Link>
+                <hr style={{ width: "98%", margin: "5px auto"}} />
+                <Link href="#" className="nav-link" onClick={closeMenu}>
+                  Ação Social
+                </Link>
+                <Link href="#" className="nav-link" onClick={closeMenu}>
+                  Escola Bíblica
+                </Link>
+                <hr style={{ width: "98%", margin: "5px auto"}} />
+                <Link href="#" className="nav-link" onClick={closeMenu}>
+                  Membros
+                </Link>
+              </div>          
+            </li>
+            <li className="nav-item">
+              <Link href="/" className="nav-link" onClick={closeMenu}>
+                Agenda
+              </Link>
+            </li>
+  
+            <li className="dropdown">
+              <a className="nav-link" href="#" onClick={closeMenu}>Contato</a>  
+              <div className="dropdown-menu">
+                <Link href="#" className="nav-link" onClick={closeMenu}>
+                  Pedidos de Oração
+                </Link>
+                <hr style={{ width: "98%", margin: "5px auto"}} />
+                <a className="nav-link" href="https://www.instagram.com/kyndermanbaraldi/" target="_blank" rel="noopener noreferrer" onClick={closeMenu}>@Insta <AiFillInstagram/> </a>
+                <a className="nav-link" href="https://www.youtube.com/@TheKynderman" target="_blank" rel="noopener noreferrer" onClick={closeMenu}>YouTube <AiFillYoutube /></a>
+              </div>          
+            </li>
+
+
+            <li className="nav-item">
+                <Link href="#" className="nav-link" onClick={closeMenu}>
+                  Blog
+                </Link>
+            </li>
+
+            <li className="nav-item">
+                <Link href="#" className="nav-link" onClick={closeMenu}>
+                  Doações
+                </Link>
+            </li>
+
+          </ul>
+        </div>
+        
+        <div className="mobile-menu">
+          <a href="#" className="mobile-menu-icon" onClick={toggleMenu}>
+            {menuOpen ? <VscMenu /> : <VscChromeClose /> }
+          </a>
+        </div>
+        
+      </nav>
+    );
+  };
+  
+  export default Menu;
