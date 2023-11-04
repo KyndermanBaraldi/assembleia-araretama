42b07a22398b0a065614e881ebf2faa57a3924e3
Kynderman Baraldi <kynderman@gmail.com>
2023-11-04 15:31:57 -0300

    add page membros

--- src/app/sobre/membros/page.tsx
+++ src/app/sobre/membros/page.tsx
@@ -1,8 +1,51 @@
+'use client'
+
+import { traceDeprecation } from "process"
+
+const membros = [
+  {"id": 1, "cargo": "PASTOR", "nome": "João da Silva"},
+  {"id": 2, "cargo": "EVANGELISTA", "nome": "Gabriel Silva"},
+  {"id": 3, "cargo": "PRESBÍTERO", "nome": "Lucas Oliveira"},
+  {"id": 4, "cargo": "PRESBÍTERO", "nome": "Mateus Santos"},
+  {"id": 5, "cargo": "DIÁCONO", "nome": "André Pereira"},
+  {"id": 6, "cargo": "DIÁCONO", "nome": "Pedro Costa"},
+  {"id": 7, "cargo": "LÍDER", "nome": "Rafael Lima"},
+  {"id": 8, "cargo": "OBREIRO", "nome": "Felipe Almeida"},
+  {"id": 9, "cargo": "OBREIRO", "nome": "Daniel Ferreira"},
+  {"id": 10, "cargo": "OBREIRO", "nome": "Matheus Oliveira"}
+]
+
 export default function Page() {
   return (
-    <div className="membros-body">
+    <div className="page-body">
 
-     Membros
+      <h1>Igreja Assembleia do Araretama</h1>
+      <h2>Membros</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>
+              Cargo
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {membros.map((membro)=> {
+            return (
+              <tr key={membro.id}>
+                <td >
+                    {membro.nome}
+                </td>
+                <td >
+                    {membro.cargo}
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
 
     </div>
   )
