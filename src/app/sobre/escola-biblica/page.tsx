f75d38aac4a0bb1989e66adbac9ce32ce6354da8
Kynderman Baraldi <kynderman@gmail.com>
2023-11-04 15:16:15 -0300

    add page escola-biblica

--- src/app/sobre/escola-biblica/page.tsx
+++ src/app/sobre/escola-biblica/page.tsx
@@ -1,8 +1,30 @@
+'use client'
+
 export default function Page() {
   return (
-    <div className="escola-biblica-body">
+    <div className="page-body">
+        <h1>Igreja Assembleia do Araretama</h1>
+        <h2>Escola Bíblica</h2>
+     
+        <p>
+          Bem-vindo à Escola Bíblica da Igreja Assembleia do Araretama, um espaço dedicado ao crescimento espiritual e à compreensão mais profunda da Palavra de Deus. Todos os domingos, convidamos você a se juntar a nós para uma jornada de aprendizado e reflexão, onde exploramos as Escrituras Sagradas em um ambiente acolhedor e amigável.
 
-     Escola Bíblica
+        </p>
+
+        <p>
+          Nossa Escola Bíblica é um lugar onde as verdades eternas se encontram com as situações cotidianas, proporcionando insights valiosos para a vida moderna. Professores dedicados compartilham seu conhecimento e paixão pelas Escrituras, conduzindo discussões interativas e estudos aprofundados sobre temas bíblicos relevantes.
+
+        </p>
+
+        <p>
+          Independentemente de você ser um estudante da Bíblia experiente ou alguém que está apenas começando sua jornada espiritual, há um lugar para você na Escola Bíblica da Igreja Assembleia do Araretama. Estamos comprometidos em criar um ambiente onde as perguntas são bem-vindas, as dúvidas são exploradas e as verdades bíblicas são celebradas.
+
+        </p>
+
+        <p>
+
+          Junte-se a nós aos domingos e permita que a Escola Bíblica seja um farol de conhecimento, fé e comunhão em sua jornada espiritual. Estamos ansiosos para compartilhar essa experiência enriquecedora com você e para crescer juntos na graça e no conhecimento de nosso Senhor e Salvador Jesus Cristo.
+        </p>
 
     </div>
   )
